ffe9bb6dd1ba006ccf69439ef8fa8424753c5a42
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-10 22:00:58 +0630

    feat: add Prisma client and configure database connection
    - Added @prisma/client and prisma as dependencies in package.json
    - Created a new database.ts file for future database configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
+const port: number = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(3000);
+app.listen(port);
+
+console.log(`APP is running on http://localhost:${port}`);
