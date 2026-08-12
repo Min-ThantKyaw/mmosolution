e7905731164e2397af135d1ce0fde3e9b0aa4adb
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-12 13:52:58 +0630

    feat: update server configuration to use environment variables for port and host

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
-const port: number = 3000;
+const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const host: string = process.env.HOST || "localhost";
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port);
+app.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
+});
 
-console.log(`APP is running on http://localhost:${port}`);
+
