1c5bd6756a7dd42cf13405ef89408969b19fa169
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-13 22:41:16 +0630

    feat: update package dependencies and clean up app initialization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,13 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express } from "express";
+import authRouter from "./routes/auth.routes.js";
 
 const app: Express = express();
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const host: string = process.env.HOST || "localhost";
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.listen(port, host, () => {
   console.log(`Server is running at http://${host}:${port}`);
 });
-
-
