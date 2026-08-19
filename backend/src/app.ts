4ef9c23ee91ea3182f0667fea056be053c4e46b6
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-20 00:08:09 +0900

    refactor: rename router variable in auth.routes and adjust app.ts for middleware usage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
 import express, { type Express } from "express";
 import "dotenv/config";
 import authRouter from "./routes/auth.routes.js";
+import adminRouter from "./routes/admin.routes.js";
 import { authenticate } from "./middlewares/auth.middleware.js";
+import { authorize } from "./middlewares/role.middleware.js";
 
 const app: Express = express();
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const host: string = process.env.HOST || "localhost";
 
 app.use(express.json());
-app.use("/api/auth", authenticate, authRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/admin", authenticate, authorize("READER"), adminRouter);
 
 app.listen(port, host, () => {
   console.log(`Server is running at http://${host}:${port}`);
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth/auth.controller.js";
 
-const router = Router();
+const authRouter: Router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/test-route", (req, res) => { 
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter .post("/test-route", (req, res) => { 
 	return res.status(200).json({ message: "Test route is working!" });
 });
 // TODO: login, forgot-password, reset-password, logout, me
 
-export default router;
+export default authRouter;
