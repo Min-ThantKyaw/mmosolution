ba15c94306bb57f26cf9e42452e2a090c65d1aab
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-25 23:08:01 +0630

    Fix: add footer design

--- web/src/components/layout/Footer.tsx
+++ web/src/components/layout/Footer.tsx
@@ -1,11 +1,13 @@
 
 
 export default function Footer() {
-  return (
+	return (
+	  <div>
 	<footer className="bg-gray-800 text-white py-4">
 	  <div className="container mx-auto text-center">
 		&copy; {new Date().getFullYear()} My Company. All rights reserved.
 	  </div>
-	</footer>
+			</footer>
+			</div>
   );
 }
