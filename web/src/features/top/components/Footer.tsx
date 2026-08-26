065df25f0ffb3a565b46b06008c575ef8af538ec
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-26 23:11:52 +0630

    feat: add Navbar, Footer, and Landing components

---
+++ web/src/features/top/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+	  <div>
+	<footer className="bg-gray-800 text-white py-4">
+	  <div className="container mx-auto text-center">
+		&copy; {new Date().getFullYear()} My Company. All rights reserved.
+	  </div>
+			</footer>
+			</div>
+  );
+}
+
+export default Footer;
---
+++ web/src/features/top/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Landing = () => {
+	return (
+		<>
+			<Navbar />
+			<Footer />
+		</>
+	)
+}
+
+export default Landing;
