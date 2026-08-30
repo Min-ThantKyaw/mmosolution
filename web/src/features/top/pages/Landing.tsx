ddc27732849313778343a61e6133805d21454390
minthant1590@gmail.com <minthant1590@gmail.com>
2026-08-30 16:09:31 +0630

    refactor: remove AuthModal, MainContent, and Navbar components to simplify authentication flow

--- web/src/features/top/pages/Landing.tsx
+++ web/src/features/top/pages/Landing.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import MainContent from "../components/MainContent";
 
 const Landing = () => {
-	const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
-
 	return (
 		<>
-			<Navbar onOpenAuthModal={() => setIsAuthModalOpen(true)} />
-			<MainContent
-				isAuthModalOpen={isAuthModalOpen}
-				onCloseAuthModal={() => setIsAuthModalOpen(false)}
-			/>
+			<Navbar />
+			<MainContent />
 			<Footer />
 		</>
 	)
