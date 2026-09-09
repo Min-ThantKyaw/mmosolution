5c5b71220537857b8415a2d3f5a0789757f8bf28
minthant1590@gmail.com <minthant1590@gmail.com>
2026-09-09 16:51:45 +0630

    feat: add axios instance for API client with default configuration

---
+++ web/src/lib/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import type { AxiosInstance } from "axios";
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api",
+  headers: {
+	"Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
+
